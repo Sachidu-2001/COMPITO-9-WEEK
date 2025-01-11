44710ce55e7f8178effe9fb257affc21c7c44b97
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-12 00:51:23 +0100

    add Header

--- app-react/src/App.jsx
+++ app-react/src/App.jsx
@@ -1,14 +1,18 @@
-import NetNavBar from "./Components/NetNavBar"
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+
+import NetFilms from "./Components/NetFilms";
+
+import NetNavBar from "./Components/NetNavBar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
   return (
     <>
-    <NetNavBar />
+      <NetNavBar />
+      <NetHeader />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
