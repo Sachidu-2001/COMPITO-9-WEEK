9dca87cf58078bf22dec7fabcf70e536925a5ea4
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-12 23:38:21 +0100

    fetch ritocchi

--- app-react/src/Components/NetFilms.jsx
+++ app-react/src/Components/NetFilms.jsx
@@ -3,30 +3,60 @@ import { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 
 const NetFilms = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies1, setMovies1] = useState([]);
+  const [movies2, setMovies2] = useState([]);
+  const [movies3, setMovies3] = useState([]);  
 
   useEffect(() => {
-    const fetchMovie = async () => {
+    const fetchMovie1 = async () => {
       try {
         const response = await fetch(
           "https://www.omdbapi.com/?apikey=a69bf1d9&s=South%20Park"
         );
         const data = await response.json();
-        setMovies(data.Search ? data.Search.slice(0, 6) : []);
+        setMovies1(data.Search ? data.Search.slice(0, 6) : []);
       } catch (error) {
         console.error("Error fetching movies:", error);
       }
     };
 
-    fetchMovie();
+    const fetchMovie2 = async () => {
+      try {
+        const response = await fetch(
+          "https://www.omdbapi.com/?apikey=a69bf1d9&s=alien"
+        );
+        const data = await response.json();
+        setMovies2(data.Search ? data.Search.slice(0, 6) : []);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+
+    const fetchMovie3 = async () => {
+      try {
+        const response = await fetch(
+          "https://www.omdbapi.com/?apikey=a69bf1d9&s=x-men"
+        );
+        const data = await response.json();
+        setMovies3(data.Search ? data.Search.slice(0, 6) : []);
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+      }
+    };
+
+    fetchMovie1();
+    fetchMovie2();
+    fetchMovie3();
+
   });
+
 
   return (
     <div className="py-2 px-3 bg-black">
       <div className="mb-3">
         <h3 className="text-white">Trending Now</h3>
         <Row className="g-1">
-          {movies.map((movie) => (
+          {movies1.map((movie) => (
             <Col
               key={movie.imdbID}
               xs={6}
@@ -44,7 +74,7 @@ const NetFilms = () => {
       <div className="mb-3">
         <h3 className="text-white">Watch It Again</h3>
         <Row className="g-1">
-          {movies.map((movie) => (
+          {movies2.map((movie) => (
             <Col
               key={movie.imdbID}
               xs={6}
@@ -62,7 +92,7 @@ const NetFilms = () => {
       <div>
         <h3 className="text-white">New Release</h3>
         <Row className="g-1">
-          {movies.map((movie) => (
+          {movies3.map((movie) => (
             <Col
               key={movie.imdbID}
               xs={6}
